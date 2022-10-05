b32c0096b1bcec27488f7c15afea234867324398
mohsen <mohamed.abdelaaty@vodafone.com>
2022-10-05 11:43:26 +0200

    adding math operation in python and lab2-2

---
+++ day2/day2-2-BMICalculator.py
@@ -0,0 +1,15 @@
+'''
+Body mass index (BMI) is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m2, resulting from mass in kilograms and height in metres.
+
+'''
+
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi=int(height)/(float(weight)**2)
+bmi_as_int=int(bmi)
+print("BMI=",bmi_as_int)
---
+++ day2/mathoperation.py
@@ -0,0 +1,18 @@
+'''
+2+3
+2*3
+2-3
+2/3
+2**3
+
+() ---> ** ----> * ----> / ----> + ---> -
+'''
+from traceback import print_tb
+
+
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+print(2**2)
+print(2//2)
